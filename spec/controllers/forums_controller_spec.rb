59d66fcaef34671bbce22e1271bd4c755c1511f2
pctao <pc_tao@htc.com>
2010-10-25 16:04:49 +0800

    forums_controller_spec add pending tests

--- spec/controllers/forums_controller_spec.rb
+++ spec/controllers/forums_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe ForumsController do
+  pending "GET index"
 
+  pending "GET show"
+
+  pending "GET new"
+
+  pending "POST create"
+
+  pending "GET edit"
+
+  pending "PUT update"
+
+  pending "DELETE destroy"
 end
