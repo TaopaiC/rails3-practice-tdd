a32f56b0a2c28c7638847287b76078d213043dbe
pctao <pc_tao@htc.com>
2010-10-25 12:23:00 +0800

    refactor forum_spec: add before :each

--- spec/models/forum_spec.rb
+++ spec/models/forum_spec.rb
@@ -11,13 +11,15 @@
 require 'spec_helper'
 
 describe Forum do
-  it "is valid with params" do
+  before(:each) do
     @params = {:title => Faker::Lorem.sentence}
+  end
+
+  it "is valid with params" do
     Forum.new(@params).should be_valid
   end
 
   it "is invalid without title" do
-    @params = {}
-    Forum.new(@params).should_not be_valid
+    Forum.new(@params.except(:title)).should_not be_valid
   end
 end
