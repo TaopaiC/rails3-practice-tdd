c3b6ced769c0a8d5713d652843e72533222bc498
pctao <pc_tao@htc.com>
2010-10-25 19:44:53 +0800

    post controller add find_forum and find_post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,10 @@
 class PostsController < ApplicationController
+  protected
+  def find_forum
+    @forum = Forum.find(params[:forum_id])
+  end
+
+  def find_post
+    @post = @forum.posts.find(params[:id])
+  end
 end
