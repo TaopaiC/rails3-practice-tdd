c6a6d555bd02e2e73ac6d02c607cb81a74b44a40
pctao <pc_tao@htc.com>
2010-10-25 19:44:21 +0800

    post_controller_spec add find_forum and find_post tests

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,7 +2,43 @@ require 'spec_helper'
 
 describe PostsController do
 
-  pending "before_filter"
+  def should_find_forum
+    @forum = mock_model(Forum)
+    controller.should_receive(:find_forum) { controller.instance_variable_set("@forum", @forum) }.ordered
+  end
+
+  def should_find_post
+    @post  = mock_model(Post)
+    controller.should_receive(:find_post ) { controller.instance_variable_set("@post",  @post)  }.ordered
+  end
+
+  describe "before_filter" do
+    it "find_forum returns requested forum" do
+      @forum = mock_model(Forum)
+      controller.params = {:forum_id => 4}
+
+      Forum.should_receive(:find).with(4).and_return(@forum)
+      controller.send(:find_forum)
+
+      assigns(:forum).should eq(@forum)
+    end
+
+    it "find_post returns requested post" do
+      @forum = mock_model(Forum)
+      @post  = mock_model(Post)
+      @posts = []
+      controller.params = {:forum_id => 4, :id => 3}
+      controller.instance_variable_set("@forum", @forum)
+      @forum.should_receive(:posts).and_return(@posts)
+      @posts.should_receive(:find).with(3).and_return(@post)
+
+      controller.send(:find_post)
+
+      assigns(:forum).should eq(@forum)
+      assigns(:post ).should eq(@post)
+    end
+  end
+
   pending "GET index"
   pending "GET show"
   pending "GET new"
