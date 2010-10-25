050c04ad7749bc9c3ee679145b468d7554b9200f
pctao <pc_tao@htc.com>
2010-10-25 12:27:31 +0800

    post_spec add tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -17,8 +17,27 @@
 require 'spec_helper'
 
 describe Post do
-  pending "is valid with params"
-  pending "is invalid without title"
-  pending "is invalid without description"
-  pending "is invalid without forum_id"
+  before(:each) do
+    @params = {
+      :title       => Faker::Lorem.sentence,
+      :description => Faker::Lorem.sentence,
+      :forum_id    => 4
+    }
+  end
+
+  it "is valid with params" do
+    Post.new(@params).should be_valid
+  end
+
+  it "is invalid without title" do
+    Post.new(@params.except(:title)).should_not be_valid
+  end
+
+  it "is invalid without description" do
+    Post.new(@params.except(:description)).should_not be_valid
+  end
+
+  it "is invalid without forum_id" do
+    Post.new(@params.except(:forum_id)).should_not be_valid
+  end
 end
