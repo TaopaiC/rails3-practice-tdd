35e8a922f7b710e4ee960a3c2808481f9203bc1b
pctao <pc_tao@htc.com>
2010-10-25 19:37:25 +0800

    post_controller_spec add pending tests

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,4 +2,13 @@ require 'spec_helper'
 
 describe PostsController do
 
+  pending "before_filter"
+  pending "GET index"
+  pending "GET show"
+  pending "GET new"
+  pending "POST create"
+  pending "GET edit"
+  pending "PUT update"
+  pending "DELETE destroy"
+
 end
